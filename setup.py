f512d811ce44ae0443e943a889f4f8875d6de5b3
William Schueller <william.schueller@gmail.com>
2015-10-20 14:48:03 +0200

    restructuration for use with pip

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import re
+import sys
+
+from setuptools import setup, find_packages
+
+def version():
+    with open('experiment_manager/_version.py') as f:
+        return re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", f.read()).group(1)
+
+def requirements():
+  with open('requirements.txt') as f:
+    return f.readlines()
+
+setup(name='naminggamesal',
+      version=version(),
+      packages=find_packages(),
+      install_requires=[requirements()],
+      author='William Schueller',
+      author_email='william.schueller@gmail.com',
+      description='Using Active Learning in Naming Games',
+      url='https://github.com/flowersteam/naminggamesal',
+      license='GNU AFFERO GENERAL PUBLIC LICENSE Version 3',
+      )
