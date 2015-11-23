36178591e985ca9eadc63d7e1338abcb4b6887a3
William <william.schueller@gmail.com>
2015-11-23 15:13:16 +0100

    tests communicative success

--- naminggamesal/ngstrat/success/communicative_success.py
+++ naminggamesal/ngstrat/success/communicative_success.py
@@ -1,7 +1,7 @@
 class CommunicativeSuccess(object):
 
 	def eval(self, ms, w, mh, voc, strategy):
-		if hearer.get_content()[ms,w] > 0:
+		if voc.get_content()[ms,w] > 0:
 			return True
 		else:
 			return False
