dd866bce9ade09848c4c88197411d2f3956a2e19
William Schueller <william.schueller@gmail.com>
2016-12-06 17:01:37 +0100

    finishing storage feature

--- additional/sqlite_storage.py
+++ additional/sqlite_storage.py
@@ -1,5 +1,6 @@
 import lzo
-
+import os
+import errno
 import cPickle
 import sqlite3 as sql
 
@@ -10,13 +11,20 @@ import sqlite3 as sql
 def add_data(filepath,data,label):
 	pickled_data = cPickle.dumps(data, cPickle.HIGHEST_PROTOCOL)
 	lz_data = lzo.compress(pickled_data)
+	try:
+		os.makedirs(os.path.dirname(filepath))
+	except OSError as exc:
+		if exc.errno == errno.EEXIST and os.path.isdir(os.path.dirname(filepath)):
+			pass
+		else:
+			raise
 	conn = sql.connect(filepath)
 	with conn:
 		cursor = conn.cursor()
 		cursor.execute("CREATE TABLE IF NOT EXISTS main_table("\
 				+"T INT, "\
 				+"Population_object BLOB)")
-		cursor.execute("INSERT INTO main_table VALUES (?,?)",(label,sql.binary(lz_data)))
+		cursor.execute("INSERT INTO main_table VALUES (?,?)",(label,sql.Binary(lz_data)))
 
 def read_data(filepath,label=None):
 	conn = sql.connect(filepath)
