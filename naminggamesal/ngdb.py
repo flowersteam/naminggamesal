78037eabe96986d0f24b3e2f190b6eec2f8961fb
William Schueller <william.schueller@gmail.com>
2017-12-10 12:59:50 +0100

    object new args issue

--- naminggamesal/ngdb.py
+++ naminggamesal/ngdb.py
@@ -37,11 +37,11 @@ class NamingGamesDB(object):
 				inst.just_retrieved = True
 				return inst
 			else:
-				instance = object.__new__()
+				instance = object.__new__(cls)
 				cls.instances.add(instance)
 				return instance
 		else:
-			instance = object.__new__()
+			instance = object.__new__(cls)
 			cls.instances.add(instance)
 			return instance
 
