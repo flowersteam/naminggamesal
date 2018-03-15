49c5f057fc76bb74305de2d262eace56faacbc11
William Schueller <william.schueller@gmail.com>
2018-03-15 15:52:16 +0100

    added export

--- naminggamesal/utils/export.py
+++ naminggamesal/utils/export.py
@@ -1,5 +1,6 @@
 import json
 import os
+import copy
 
 def export_agent_past(xp,agent_number=0,file=None):
     if file is None:
@@ -9,4 +10,11 @@ def export_agent_past(xp,agent_number=0,file=None):
     if not os.path.exists('json_data'):
         os.makedirs('json_data')
     with open('json_data/'+filename,'w') as f:
-        f.write(json.dumps(xp._poplist.get_last()._agentlist[agent_number]._memory['past_interactions_all']))
+        try:
+            f.write(json.dumps(xp._poplist.get_last()._agentlist[agent_number]._memory['past_interactions_all']))
+        except TypeError:
+            new_dict_list = copy.deepcopy(xp._poplist.get_last()._agentlist[agent_number]._memory['past_interactions_all'])
+            for elt in new_dict_list:
+                for k in elt.keys():
+                    elt[k] = str(elt[k])
+            f.write(json.dumps(new_dict_list))
