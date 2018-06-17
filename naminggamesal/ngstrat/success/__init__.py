055209d30938f84c38e40c03ea0110b6c3c8715c
William Schueller <william.schueller@gmail.com>
2018-06-17 23:20:40 +0200

    global_norandom success as default

--- naminggamesal/ngstrat/success/__init__.py
+++ naminggamesal/ngstrat/success/__init__.py
@@ -17,7 +17,7 @@ success_class={
 	'acceptancepolicyornormal_success':'acceptancepolicy_success.AcceptancePolicyOrNormalSuccess',
 }
 
-def get_success(success_type='global', **success_cfg2):
+def get_success(success_type='global_norandom', **success_cfg2):
 	tempint = success_type
 	if tempint in list(success_class.keys()):
 		tempint = success_class[tempint]
