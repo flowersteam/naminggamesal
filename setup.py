11de0620d4fe614f9ebe3b5cea1b84e848c3a9df
William Schueller <william.schueller@gmail.com>
2018-06-21 11:05:48 +0200

    cython install bug fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 import subprocess
 import sys
 
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Extension
 try:
     from Cython.Build import cythonize
 except ImportError:
@@ -25,11 +25,14 @@ setup(name='naminggamesal',
       version=version(),
       packages=find_packages(),
       install_requires=[requirements()],
-      setup_requires=['Cython', ],
+      setup_requires=['setuptools>=18.0','Cython', ],
       author='William Schueller',
       author_email='william.schueller@gmail.com',
       description='Using Active Learning in Naming Games',
       url='https://github.com/flowersteam/naminggamesal',
       license='GNU AFFERO GENERAL PUBLIC LICENSE Version 3',
-      ext_modules = cythonize(['naminggamesal/ngmeth_utils/csrtheo_utils.pyx','naminggamesal/ngvoc/c2dictdict.pyx']),
-      )
+      #ext_modules = cythonize(['naminggamesal/ngmeth_utils/csrtheo_utils.pyx','naminggamesal/ngvoc/c2dictdict.pyx']),
+      ext_modules=
+        [Extension(sources=['naminggamesal/ngmeth_utils/csrtheo_utils.pyx','naminggamesal/ngvoc/c2dictdict.pyx']),]
+    )
+
