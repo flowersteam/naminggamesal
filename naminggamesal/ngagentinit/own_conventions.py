8390594b6f51c7fe5f9d7b7ec94e163848b200ac
William Schueller <william.schueller@gmail.com>
2018-06-25 17:33:51 +0200

    improved own_words agentinit

--- naminggamesal/ngagentinit/own_conventions.py
+++ naminggamesal/ngagentinit/own_conventions.py
@@ -2,13 +2,18 @@ from .agent_init import AgentInit
 
 class OwnWordsInit(AgentInit):
 
-	def __init__(self,W_l=None,W_range=None):
+	def __init__(self,W_l=None,W_range=None,M=1):
 		if W_l is None:
 			self.W_l = list(range(W_range))
 		else:
 			self.W_l = W_l
+		self.M = M
 
 	def modify_agent(self,agent,pop,pop_init=False):
 		AgentInit.modify_agent(self,agent=agent,pop=pop,pop_init=pop_init)
 		if self.W_l:
-			agent._vocabulary.next_word = self.W_l.pop(0)
+			if not hasattr(self,'M') or self.M == 1:
+				agent._vocabulary.next_word = self.W_l.pop(0)
+			else:
+				agent._vocabulary.next_word = self.W_l[:self.M]
+				self.W_l = self.W_l[self.M:]
