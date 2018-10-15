099990040db0f8418e554c964457c7db5b28d53f
William Schueller <william.schueller@gmail.com>
2018-10-15 14:45:45 +0200

    script tab n_chunks

--- naminggamesal/ngmeth_utils/conv_utils.py
+++ naminggamesal/ngmeth_utils/conv_utils.py
@@ -25,7 +25,7 @@ def inv2_c(N):
 	if N < 19:
 		return 1 #Computation issue for N <19
 	else:
-		for i in xrange(10**10):
+		for i in xrange(10**8):
 			c = iter_c(c)
 			if c <= 1.:
 				if c == 1.:
@@ -42,13 +42,25 @@ def iter_M(x,N):
 
 def minexplo_per_ag(M,N):
 	m = M
-	for i in xrange(10**10):
+	for i in xrange(10**6):
 		m = iter_M(m,N)
 		if m <= 1.:
 			if m == 1.:
 				return (i + 1)/2.
 			else:
 				return i/2.
+	raise ValueError('Computation too long, cannot get convergence time')
+
+def nchunks(M,N):
+	m = M
+	for i in xrange(10**10):
+		m = iter_M(m,N)
+		if m <= 1.:
+			if m == 1.:
+				ans = i#(i + 1)
+			else:
+				ans = i
+			return max(ans,1)
 	raise ValueError('Computation too long, cannot get convergence time')
 
 def extra_inv(M,N):
@@ -122,30 +134,46 @@ def perf1_memmax(memmax,M,N):
 	return memax_optimal(N=N,M=M)*1./memmax
 
 if __name__ == '__main__':
-	scale_vec = [10,20,50,100,500,1000,2000,5000,10000,20000,50000,100000]
-	import matplotlib.pyplot as plt
-	for N in [10,100,1000,10000,100000]:
-		Y = [minexplo_per_ag(M,N)/M for M in scale_vec]
-		plt.plot(scale_vec,Y,label='N='+str(N))
-	plt.legend()
-	plt.xlabel('M')
-	plt.show()
-	for M in [10,100,1000,10000,100000]:
-		Y = [minexplo_per_ag(M,N) for N in scale_vec]
-		plt.plot(scale_vec,Y,label='M='+str(M))
-	plt.xlabel('N')
-	plt.legend()
-	plt.show()
-	for coeff in [0.1,0.5,1.,2.,10.]:
-		Y = [minexplo_per_ag(coeff*N,N) for N in scale_vec]
-		plt.plot(scale_vec,Y,label='M='+str(coeff)+'*N')
-	plt.xlabel('N')
-	plt.legend()
-	plt.show()
-	for coeff in [0.1,0.5,1.,2.,10.]:
-		Y = [(minexplo_per_ag(coeff*N,N)*N)-N*coeff for N in scale_vec]
-		plt.plot(scale_vec,Y,label='M='+str(coeff)+'*N')
-	plt.xlabel('N')
-	plt.legend()
-	plt.show()
+	# scale_vec = [10,20,50,100,500,1000,2000,5000,10000,20000,50000,100000]
+	# import matplotlib.pyplot as plt
+	# for N in [10,100,1000,10000,100000]:
+	# 	Y = [minexplo_per_ag(M,N)/M for M in scale_vec]
+	# 	plt.plot(scale_vec,Y,label='N='+str(N))
+	# plt.legend()
+	# plt.xlabel('M')
+	# plt.show()
+	# for M in [10,100,1000,10000,100000]:
+	# 	Y = [minexplo_per_ag(M,N) for N in scale_vec]
+	# 	plt.plot(scale_vec,Y,label='M='+str(M))
+	# plt.xlabel('N')
+	# plt.legend()
+	# plt.show()
+	# for coeff in [0.1,0.5,1.,2.,10.]:
+	# 	Y = [minexplo_per_ag(coeff*N,N) for N in scale_vec]
+	# 	plt.plot(scale_vec,Y,label='M='+str(coeff)+'*N')
+	# plt.xlabel('N')
+	# plt.legend()
+	# plt.show()
+	# for coeff in [0.1,0.5,1.,2.,10.]:
+	# 	Y = [(minexplo_per_ag(coeff*N,N)*N)-N*coeff for N in scale_vec]
+	# 	plt.plot(scale_vec,Y,label='M='+str(coeff)+'*N')
+	# plt.xlabel('N')
+	# plt.legend()
+	# plt.show()
+	N_l = [10,100,1000]
+	Nc_l = [0.2,0.3,0.5,0.7,1.,2.,5,10]
+	M_l = [100,1000,10000]
+	for M in M_l:
+		for Nc in Nc_l:
+			N = int(Nc*M)
+			val = int(2*M/N)+ 1
+			print('N',N,'M',M,'2M/N+1',val,'nch',nchunks(M,N))
+			print(val-nchunks(M,N))
+	print('tab')
+	for M in M_l:
+		for Nc in Nc_l:
+			N = int(Nc*M)
+			val = int(2*M/N)+1
+			print(N,'&',M,'&',val,'&',nchunks(M,N), '\\\\')
+			# print(val-nchunks(M,N))
 
--- naminggamesal/tools/efficient_minimal_ng.py
+++ naminggamesal/tools/efficient_minimal_ng.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+import random
 import time
 from collections import defaultdict
 from memory_profiler import memory_usage
@@ -8,6 +9,118 @@ def stop_condition(T,pop,Tmax,monitor_T):
 	if T == monitor_T:
 		print(memory_usage())
 	return (T >= Tmax) or (T>0 and len(pop) == 1)
+
+def new_stop_condition(T,pop,Tmax,monitor_T):
+	if T == monitor_T:
+		print(memory_usage())
+	return (T >= Tmax) or (T>0 and len(pop['d1']) == 1)
+
+def pop_add(ag,pop):
+	s = pop['d1'][ag]+1
+	pop['d1'][ag] = s
+	if s > 1:
+		pop['d2'][s-1].remove(ag)
+		pop['d2bis'][s-1]-=1
+		if pop['d2bis'][s-1] == 0:
+			del pop['d2bis'][s-1]
+			del pop['d2'][s-1]
+	pop['d2bis'][s]+=1
+	pop['d2'][s].add(ag)
+	pop['N'] += 1
+
+def pop_rm(ag,pop):
+	s = pop['d1'][ag]-1
+	if s > 0:
+		pop['d1'][ag] = s
+
+		pop['d2bis'][s] += 1
+		pop['d2'][s].add(ag)
+	else:
+		del pop['d1'][ag]
+	pop['d2'][s+1].remove(ag)
+	pop['d2bis'][s+1]-=1
+	if pop['d2bis'][s+1] == 0:
+		del pop['d2bis'][s+1]
+		del pop['d2'][s+1]
+	pop['N'] -= 1
+
+
+def pop_init(N):
+	pop = {'d1':defaultdict(int),'d2':defaultdict(set),'d2bis':defaultdict(int)}
+	pop['d1'][()] = N
+	pop['d2'][N].add(())
+	pop['d2bis'][N] = 1
+	pop['N'] = float(N)
+	return pop
+
+def pick_size(pop):
+	keys = list(pop['d2bis'].keys())
+	# print(keys)
+	if len(keys) == 1:
+		return keys[0]
+	# keys = np.asarray(keys)
+	values = [k*pop['d2bis'][k]/pop['N'] for k in keys]
+	try:
+		s = np.random.choice(keys,p=values)
+	except:
+		print(values)
+		print(pop['d2bis'])
+		print(sum(values))
+		raise
+	# print(values)
+	return s
+
+def new_pick_agent(pop):
+	s = pick_size(pop=pop)
+	# print(s)
+	# print(pop['d2'],'blah')
+	ag = random.sample(pop['d2'][s],1)[0]
+	pop_rm(ag=ag,pop=pop)
+	return ag
+
+def new_run(Tmax,N):
+	try:
+		pop = pop_init(N=N)
+		max_word = -1
+		monitor_T = int(1./np.sqrt(2.)*N**(1.5))
+		T = 0
+		while not new_stop_condition(T,pop,Tmax,N):
+			sp = new_pick_agent(pop)
+			hr = new_pick_agent(pop)
+			if sp == ():
+				max_word += 1
+				sp = (max_word,)
+				# hr = tuple(sorted((*hr,max_word)))
+				hr = tuple(sorted(hr+(max_word,)))
+			else:
+				w = np.random.choice(sp)
+				if w in hr:
+					sp = (w,)
+					hr = (w,)
+				else:
+					# hr = tuple(sorted((*hr,max_word)))
+					hr = tuple(sorted(hr+(max_word,)))
+			pop_add(pop=pop,ag=sp)
+			pop_add(pop=pop,ag=hr)
+			T += 1
+		print('end',memory_usage())
+		return T
+	except:
+		print('T',T)
+		print('mem',memory_usage())
+		print('len d1',len(pop['d1']))
+		print('len d2',len(pop['d2']))
+		raise
+
+
+
+
+
+#################
+
+
+
+
 
 def pick_agent(pop):
 	p = np.asarray(list(pop.values()),dtype=float)
@@ -28,6 +141,7 @@ def pick_agent(pop):
 		del pop[ans]
 	return ans
 
+
 def run(Tmax,N):
 	try:
 		pop = defaultdict(int)
@@ -41,7 +155,8 @@ def run(Tmax,N):
 			if sp == ():
 				max_word += 1
 				sp = (max_word,)
-				hr = tuple(sorted((*hr,max_word)))
+				# hr = tuple(sorted((*hr,max_word)))
+				hr = tuple(sorted(hr+(max_word,)))
 				if pop[()] == 0:
 					del pop[()]
 				pop[sp] += 1
@@ -51,7 +166,8 @@ def run(Tmax,N):
 				if w in hr:
 					pop[(w,)] += 2
 				else:
-					hr = tuple(sorted((*hr,max_word)))
+					hr = tuple(sorted(hr+(max_word,)))
+					# hr = tuple(sorted((*hr,max_word)))
 					pop[sp] += 1
 					pop[hr] += 1
 			T += 1
@@ -61,13 +177,16 @@ def run(Tmax,N):
 		print('T',T)
 		print('mem',memory_usage())
 		print('len',len(pop))
+		raise
 
 
 if __name__ == '__main__':
 	start = time.time()
 	print(memory_usage())
-	N=2000
+	N=10000
 	print('N',N)
-	t = run(Tmax = 10000000, N=N)
-	print(t)
-	print(time.time()-start,'seconds')
+	try:
+		t = new_run(Tmax=100000, N=N)
+		print(t)
+	finally:
+		print(time.time()-start,'seconds')
